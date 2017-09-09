1eb9a081396a92bb56c40900850730e0033ba970
diekmann <diekmann@net.in.tum.de>
2017-09-09 21:44:12 +0200

    beeing specific for our specific example

--- main.c
+++ main.c
@@ -16,10 +16,15 @@ void sa_sigsegv(int signum, siginfo_t *siginfo, void *ucontext_void){
     ucontext_t *ucontext = ucontext_void;
     assert(signum == siginfo->si_signo);
     assert(signum == SIGSEGV);
-    printf("Handling SIGSEGV\n");
+    printf("Handling SIGSEGV at %p\n", siginfo->si_addr);
+
+    // only for invalid_ptr exaple
+    assert(siginfo->si_code == SEGV_MAPERR); // address not mapped
+    assert(siginfo->si_addr == 0x42);
+    // end invalid_ptr example
 }
 
-bool has_signalhandler(const struct sigaction * const act){
+bool has_signalhandler(const struct sigaction *const act){
     bool ret = 0;
     if((act->sa_flags & SA_SIGINFO) && (act->sa_sigaction != NULL)){
         printf("sa_sigaction at %p\n", act->sa_sigaction);
@@ -45,8 +50,9 @@ int main(int argc, char** argv){
     doErrExit(sigaction(SIGSEGV, &act, &oldact));
     assert(!has_signalhandler(&oldact)); //no old signalhandler overwritten
 
+    // check that our handler is now propperly installed
     doErrExit(sigaction(SIGSEGV, NULL, &oldact));
-    assert(has_signalhandler(&oldact)); // check that our handler is now propperly installed
+    assert(has_signalhandler(&oldact));
 
     asm volatile ("" : : : "memory"); // barrier
     int * invalid_ptr = 0x42;
